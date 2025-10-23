7666234203c33d469a98d43eaafc66555303ddf0
saqib <realhussainsakib@gmail.com>
2025-10-23 11:38:31 +0000

    fixed the structure for monorepo, might break as well, not suree

--- poc-1/frontend/src/custom.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This file tells TypeScript to trust the vis-network module,
- * even though it doesn't have its own official type declarations.
- * This resolves the "Could not find a declaration file" error.
- */
-declare module 'vis-network/dist/vis-network.esm.min.js';
